2c35d2862e65c620d5e8d980a3bf7325126f9c2b
Aditya01229 <adityajasoriya2003@gmail.com>
2024-07-28 02:33:23 +0530

    add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }))
+  const config = new DocumentBuilder()
+    .setTitle('Todo REST API')
+    .setDescription('A rest API for a todo application')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
   await app.listen(3000);
 }
 bootstrap();
--- backend/src/search/search.controller.ts
+++ backend/src/search/search.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get, Param, BadRequestException } from "@nestjs/common";
 import { SearchService } from "./search.service";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
+@ApiTags('Search')
 @Controller('api/search')
 export class SearchController {
     constructor(private readonly searchService: SearchService) {}
 
+    @ApiOperation({ description:'Enter BlockHash or BlockHeight.', summary: 'Search for Block.' })
     @Get('block/:search')
     async block(@Param('search') search: string) {
         if (!search || typeof search !== 'string') {
@@ -18,6 +21,7 @@ export class SearchController {
         }
     }
 
+    @ApiOperation({ description:'Enter Transaction.', summary: 'Search for Transaction.' })
     @Get('transaction/:search')
     async transaction(@Param('search') search: string) {
         if (!search || typeof search !== 'string') {
@@ -27,6 +31,7 @@ export class SearchController {
         return this.searchService.searchByTransaction(search);
     }
 
+    @ApiOperation({ description:'Enter Address.', summary: 'Search for Address.' })
     @Get('address/:search')
     async address(@Param('search') search: string) {
         if (!search || typeof search !== 'string') {
@@ -37,6 +42,7 @@ export class SearchController {
     }
 
     @Get('*')
+    @ApiOperation({ description:'To handel other invalid url', summary: 'Handel Invalid URL' })
     async handleAll() {
         throw new BadRequestException('Invalid route');
     }
